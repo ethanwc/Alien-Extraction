e7d00231d4ce6607f0f6bff45977b88f5194172a
ethanwc <ethanwcheatham@gmail.com>
2019-01-22 16:40:41 -0800

    laser beam detection

--- ML/main.js
+++ ML/main.js
@@ -1,6 +1,6 @@
 var AM = new AssetManager();
 
-var scale = 1/8;
+var scale = 1/16;
 var ufoscale = 3;
 var u;
 var mousex = 0, mousey = 0;
@@ -60,7 +60,62 @@ Laser.prototype.constructor = Laser;
 Laser.prototype.update = function () {
     Entity.prototype.update.call(this);
 
+    //Detect Laser collision with tiles
+    //Reference: http://www.jeffreythompson.org/collision-detection/line-rect.php
+
+    for (var i = 0; i < this.game.entities.length; i++) {
+        var ent = this.game.entities[i];
+
+        if (ent instanceof Tile) {
+
+
+            var x1 = u.x + 28 * ufoscale, y1 = u.y + 37 * ufoscale;
+            var x2 = mousex, y2 = mousey;
+
+            var rx = ent.x;
+            var ry = ent.y;
+
+            var rw = ent.animation.frameWidth * scale;
+            var rh = ent.animation.frameHeight * scale;
+
+            var left =   lineLine(x1,y1,x2,y2, rx,ry,rx, ry+rh);
+            var right =  lineLine(x1,y1,x2,y2, rx+rw,ry, rx+rw,ry+rh);
+            var top =    lineLine(x1,y1,x2,y2, rx,ry, rx+rw,ry);
+            var bottom = lineLine(x1,y1,x2,y2, rx,ry+rh, rx+rw,ry+rh);
+
+            if (left || right || top || bottom)
+                ent.removeFromWorld = true;
+
+            }
+        }
+
+
+
 };
+
+// LINE/LINE
+function lineLine(x1, y1, x2, y2, x3, y3, x4, y4) {
+
+    // calculate the direction of the lines
+    var uA = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+    var uB = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+
+    // if uA and uB are between 0-1, lines are colliding
+    if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
+
+
+        // optionally, draw a circle where the lines meet
+        // var intersectionX = x1 + (uA * (x2-x1));
+        // var intersectionY = y1 + (uA * (y2-y1));
+
+        // fill(255,0,0);
+        // noStroke();
+        // ellipse(intersectionX, intersectionY, 20, 20);
+
+        return true;
+    }
+    return false;
+}
 
 Laser.prototype.draw = function (event) {
     Entity.prototype.draw.call(this);
