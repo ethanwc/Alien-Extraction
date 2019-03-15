0008a692fab0262f8a77a81d2b55afe290f1f9d9
ethanwc <ethanwc@u.washington.edu>
2019-03-15 16:49:27 -0700

    elevation fix

--- Model/Model.js
+++ Model/Model.js
@@ -37,7 +37,7 @@ class Model {
 
 
     update() {
-        this.elevation =150 + -1 * ((ship.y + ship.w * .5)/10 - 2 | 0);
+        this.elevation = 150 + -38 -1 * ((ship.y + ship.w * .5)/10 - 2 | 0);
         this.string = "Balance: " + this.balance + "  Elevation: " + this.elevation +
             "  Capacity: " + ((this.cargo / this.cargoCapacity) * 100 | 0) + "%";
         this.checkFuel();
